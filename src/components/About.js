2734b0b221061229368e9f6918cf4567c722c0ee
Bryant Huang <huangbrynt@gmail.com>
2023-04-06 14:29:16 -0700

    update styling for mobile view

--- src/components/About.js
+++ src/components/About.js
@@ -9,11 +9,6 @@ function About() {
         <p>What sets me apart as a realtor is my experience in home renovations. With my keen eye for design and my knowledge of the local real estate market, I can help you see the potential in a property and guide you on making smart renovation decisions that add value to your investment. Whether you're looking to buy a fixer-upper, sell a home that needs a little TLC, or renovate your new home to suit your lifestyle, I'm here to provide the guidance and expertise you need.</p>
 
         <p>My devotion to my clients stems from a deep empathy and astute listening skills, as well as an intimate market knowledge that allows me to craft personalized, winning strategies for every client. As your trusted advisor, I'm committed to ensuring that you have all the information and resources you need to make informed decisions and achieve your real estate goals. With my dedication, expertise, and passion for the Bay Area and its residents, I'm confident that I can help you find the perfect home and make it your own.</p>
-        {/* <ul class=icon-list>
-            <li class=icon><a class=linkedin href=https://www.linkedin.com/in/lily-lai-03382a87 target="_blank"></a></li>
-            <li class=icon><a class=facebook href=https://www.instagram.com/lily_lai__ target="_blank"></a></li>
-            <li class=icon><a class=instagram href=https://www.instagram.com/lily_lai__ target="_blank"></a></li> 
-        </ul> */}
     </div>
   );
 }
